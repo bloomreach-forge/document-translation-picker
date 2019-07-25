b3408539bff5cc41e27ca95583a885b0200bcf61
Marijan Milicevic <marijan.milicevic@bloomreach.com>
2019-07-25 12:01:19 +0200

    HIPPLUG-1596 Upgrade Document Translation Picker to v13
    - delete old demo
    - fix build

--- demo/site/src/main/java/org/onehippo/forge/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.forge.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="documenttranslationpickerdemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
