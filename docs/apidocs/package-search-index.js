ff1ae01cd91b5db294bbd0ae4d68a15e215210a4
marijan milicevic <marijan.milicevic@bloomreach.com>
2024-08-07 13:50:30 +0200

    FORGE-527 Upgrade Document Translation Picker for BrXM v16
    - new docs

--- docs/apidocs/package-search-index.js
+++ docs/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.onehippo.forge.documenttranslationpicker.plugin"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.onehippo.forge.documenttranslationpicker.plugin"}];updateSearchResults();
--- docs/apidocs/type-search-index.js
+++ docs/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.onehippo.forge.documenttranslationpicker.plugin","l":"LinkTranslationsWorkflowPlugin"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.onehippo.forge.documenttranslationpicker.plugin","l":"LinkTranslationsWorkflowPlugin"}];updateSearchResults();
